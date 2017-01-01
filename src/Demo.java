1a1faa755db04ac64719c8b81a7d1b2e4dd9df00
chriswongatcuhk <wc38.wong@gmail.com>
2017-01-01 09:29:26 +0800

    sort cards by kind

--- src/Demo.java
+++ src/Demo.java
@@ -3,7 +3,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		PokerHand hand1 = new PokerHand("KS 2H 5C JD TD");
-		PokerHand hand2 = new PokerHand("KS 2H 5C JD TD");
+		PokerHand hand2 = new PokerHand("AS 2H 5C JD TD");
 		
 		System.out.println(hand1.compareWith(hand2));
 		
--- src/PokerHand.java
+++ src/PokerHand.java
@@ -1,15 +1,60 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Comparator;
 
+
+/**
+ * 
+ * @author chriswong
+ *
+ */
 public class PokerHand {
-	public PokerHand(String cards) {
+	private final String[] KINDS = {"2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"};
+	private final String[] SUITS = {"S", "H", "D", "C"};
 
+	/**
+	 * Constructor
+	 * @param cards
+	 */
+	public PokerHand(String cardsString) {
+		// cards = split the hand by space " "
+		String[] cards = cardsString.split(" ");
+		
+		// sort cards by kind
+		Arrays.sort(cards, new Comparator<String>(){
+
+			/**
+			 * Sort by the kind
+			 * @param o1
+			 * @param o2
+			 * @return
+			 */
+			public int compare(String o1, String o2) {
+				// get the first character(as String), find the corresponding index in KINDS
+				int kind1 = Arrays.asList(KINDS).indexOf(o1.substring(0, 1));
+				int kind2 = Arrays.asList(KINDS).indexOf(o2.substring(0, 1)); 
+				return kind1 - kind2;
+			}
+			
+		});		
 	}
 	
+	/**
+	 * Compare this hand with another hand
+	 * @param	hand another hand
+	 * @return	result
+	 */
 	public Result compareWith(PokerHand hand){
 		return Result.WIN;
 	}
+	
 }
 
-
+/**
+ * Rank
+ * @author chriswong
+ *
+ */
 interface Rank{
 	public int Ranking = 0;
 	public int compare(Rank rank);
