d189757c500c94981f4131d65ecd74c9e8992ea8
chriswongatcuhk <wc38.wong@gmail.com>
2017-01-01 22:59:17 +0800

    RUN EXAMPLES

--- src/Tests.java
+++ src/Tests.java
@@ -9,70 +9,83 @@ public class Tests
     private PokerHand.Result tie = PokerHand.Result.TIE;
     
     @Test
-    public void HighestStraightFlushWins()
-    {
+    public void HighestStraightFlushWins(){
         Test("Highest straight flush wins", loss, "2H 3H 4H 5H 6H", "KS AS TS QS JS");
     }
     
     @Test
-    public void StraightFlushWinsFourCardsOfKind()
-    {
+    public void StraightFlushWinsFourCardsOfKind(){
     	Test("Straight flush wins 4 of a kind", win, "2H 3H 4H 5H 6H", "AS AD AC AH JD");
     }
     
     @Test
-    public void HighestFourCardsOfKindWins()
-    {
+    public void HighestFourCardsOfKindWins(){
     	Test("Highest 4 of a kind wins", win, "AS AH 2H AD AC", "JS JD JC JH 3D");
     }
     
     @Test
-    public void FourCardsOfKindWinsFullHouse()
-    {
+    public void FourCardsOfKindWinsFullHouse(){
         Test("4 Of a kind wins of full house", loss, "2S AH 2H AS AC", "JS JD JC JH AD");
     }
     
     @Test
-    public void FullHouseWinsFlush()
-    {
+    public void FullHouseWinsFlush(){
     	Test("Full house wins of flush", win, "2S AH 2H AS AC", "2H 3H 5H 6H 7H");
     }
     
     @Test
-    public void HighestFlushWins()
-    {
+    public void HighestFlushWins(){
         Test("Highest flush wins", win, "AS 3S 4S 8S 2S", "2H 3H 5H 6H 7H");
     }
 
     @Test
-    public void FlushWinsStraight()
-    {
+    public void FlushWinsStraight(){
         Test("Flush wins of straight", win, "2H 3H 5H 6H 7H", "2S 3H 4H 5S 6C");
     }
     
     @Test
-    public void EqualStraightIsTie()
-    {
+    public void EqualStraightIsTie(){
 	      Test("Equal straight is tie", tie, "2S 3H 4H 5S 6C", "3D 4C 5H 6H 2S");
     }
     
     @Test
-    public void StraightWinsThreeCardsOfKind()
-    {
+    public void StraightWinsThreeCardsOfKind(){
         Test("Straight wins of three of a kind", win, "2S 3H 4H 5S 6C", "AH AC 5H 6H AS");
     }
     
     @Test
-    public void ThreeCardsOfKindWinsTwoPairs()
-    {
+    public void ThreeCardsOfKindWinsTwoPairs(){
         Test("3 Of a kind wins of two pair", loss, "2S 2H 4H 5S 4C", "AH AC 5H 6H AS");
     }
      
     @Test
-    public void TwoPairsWinsOnePair()
-    {
+    public void TwoPairsWinsOnePair(){
         Test("2 Pair wins of pair", win, "2S 2H 4H 5S 4C", "AH AC 5H 6H 7S");
-
+    }
+    
+    @Test
+    public void HighestPairWins(){
+    	Test("Highest pair wins", loss, "6S AD 7H 4S AS", "AH AC 5H 6H 7S");
+    }
+    
+    @Test
+    public void PairWinsNothing(){
+        Test("Pair wins of nothing", loss, "2S AH 4H 5S KC", "AH AC 5H 6H 7S");
+    }
+    
+    @Test
+    public void HighestCardLoses(){
+        Test("Highest card loses", loss, "2S 3H 6H 7S 9C", "7H 3C TH 6H 9S");
+    }
+    
+    @Test
+    public void HighestCardWins(){
+        Test("Highest card wins", win, "4S 5H 6H TS AC", "3S 5H 6H TS AC");
+    }
+    
+    @Test
+    public void EqualCardsTies(){
+	    Test("Equal cards is tie", tie, "2S AH 4H 5S 6C", "AD 4C 5H 6H 2C");
     }
     
     private void Test(String description, PokerHand.Result expected, String playerHand, String opponentHand)
